da8aedf2bf5ccb6f942e0d8822a44847175c402a
Piyal-Banik <piyalbanik2@gmail.com>
2026-06-04 20:36:37 +0530

    created fixed length chunker class

---
+++ chunkers/fixed_chunker.py
@@ -0,0 +1,32 @@
+# Simple Character based chunking
+
+from .base import BaseChunker
+
+class FixedChunker(BaseChunker):
+    def __init__(
+        self,
+        chunk_size: int = 512,
+        chunk_overlap: int = 50
+    ):
+        
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def chunk(
+        self,
+        text: str
+    ) -> list[str]:
+        
+        chunks = []
+        start = 0
+
+        while start < len(text):
+            
+            end = start + self.chunk_size
+
+            chunks.append(text[start:end])
+
+            start += self.chunk_size - self.chunk_overlap
+
+        return chunks
+        
